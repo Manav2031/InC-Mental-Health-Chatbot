aa1146b27df05d7ad8867854973dc2377a4c291e
varadpundlik <varadpundlik@gmail.com>
2023-08-13 10:51:23 +0530

    added tfjs model

--- server/controllers/alert.js
+++ server/controllers/alert.js
@@ -1,10 +1,25 @@
-const { execSync } = require('child_process');
+const tf=require('@tensorflow/tfjs')
+const path = require('path');
+const fetch = require('node-fetch'); // Import the polyfill
 
-exports.callEmotionDetection=(text)=> {
-    const command = `python alertController.py "${text}"`;
-    const result = execSync(command);
-    return parseInt(result.toString().trim());
-  }
-  
+// Monkey-patch TensorFlow.js to use node-fetch
+tf.util.fetch = fetch;
+
+async function loadModel() {
+    const modelPath = path.join(__dirname, 'server/tfjs_model');
+    console.log(modelPath)
+    const model = await tf.loadLayersModel(`file://${modelPath}/model.json`);
+    return model;
+}
+
+// Call the loadModel function
+loadModel()
+    .then(model => {
+        console.log('Model loaded successfully:', model);
+        // Use the loaded model for inference or other tasks
+    })
+    .catch(error => {
+        console.error('Error loading the model:', error);
+    });
 
   
